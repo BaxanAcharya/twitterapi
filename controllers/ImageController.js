df955537c156a6681e6e7acfdc5f93419e24b002
BaxanAcharya <baxanacharya@gmail.com>
2020-01-07 10:05:00 +0545

    image validation while registration

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -2,7 +2,15 @@
 function insertImage(req,res)
 {
     //console.log(req.file.filename)
-    res.json(req.file)
+    if (!req.file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+    	res.json({
+    		status:500,
+    		message:'File Format not supported'
+    	})
+    }else{
+    	 res.json(req.file)
+    }
+   
 }
 
 
